33d20ad37ed79f7e807020900cac3702800a957c
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-30 11:01:03 +0500

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -58,12 +58,12 @@ class PostsController < ApplicationController
 
   private
 
-    # Use callbacks to share common setup or constraints between actions.
+  # Use callbacks to share common setup or constraints between actions.
   def set_post
     @post = Post.find(params[:id])
   end
 
-    # Only allow a list of trusted parameters through.
+  # Only allow a list of trusted parameters through.
   def post_params
     params.require(:post).permit(:post)
   end
