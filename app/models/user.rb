1fb0c2a00e8c62fa6b825318d31ec1a6e07af01f
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-06-30 09:58:38 +0500

    Show profile on index page

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :username, presence: true
   validates :password, presence: true
+  validates :email, presence: true
   validates :password_confirmation, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
